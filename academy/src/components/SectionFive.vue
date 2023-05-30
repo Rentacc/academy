d5eb694cf957c360c474103aa3db0b3fd414d1b8
rentacc <rtcommercialert@gmail.com>
2023-05-30 20:08:45 +0200

    main structure completed

--- academy/src/components/SectionFive.vue
+++ academy/src/components/SectionFive.vue
@@ -1,6 +1,18 @@
 <script></script>
 
-<template></template>
+<template>
+    <div class="container">
+        <img src="public/img/gallery-1-3(1).jpg" alt="">
+        <img src="public/img/gallery-2-3(1).jpg" alt="">
+        <img src="public/img/gallery-3-3(1).jpg" alt="">
+        <img src="public/img/news-3-2.jpg" alt="">
+    </div>
+</template>
 
 
-<style scoped></style>
+<style scoped>
+img{
+    width: 375px;
+    height: 255.91px;
+}
+</style>
