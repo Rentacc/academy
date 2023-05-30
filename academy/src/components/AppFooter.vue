724f2fcbc223c94b5bf208ebf375e9aba220a7da
rentacc <rtcommercialert@gmail.com>
2023-05-30 21:15:41 +0200

    add footer

--- academy/src/components/AppFooter.vue
+++ academy/src/components/AppFooter.vue
@@ -1,11 +1,248 @@
 <script>
+document.addEventListener("DOMContentLoaded", function(){
+      var el = document.querySelector(".button-bird");
+      var text = document.querySelector(".button-bird__text");
+          el.addEventListener('click', function() {
+            el.classList.toggle('active');
 
+            if(el.classList.contains('active')){
+            	console.log('true');
+            	text.innerHTML = 'DONE';
+            }else{
+            	text.innerHTML = 'SEND';
+            }
+        });
+    });
 </script>
 
 <template>
-    <h2>FOOTER</h2>
+    <footer>
+
+        <article>
+
+<form id="form-newsletter">
+  <input type="text" placeholder="Name" name="name">
+  
+  <input type="text" placeholder="Email" name="email">
+
+  <button data-type="submit">
+    <i class="fa fa-paper-plane"></i>
+    Submit To Newsletter
+  </button>
+</form>
+
+</article>
+
+
+    
+      
+    <div class="footer-departments">
+      <div class="footer-department">
+        <h4>Contact us</h4>
+        <p>900 Wood Street, MI New York 48607</p>
+        <p>345758000-09</p>
+        <p>info@example.com</p>
+      </div>
+      <div class="footer-department">
+        <h4>Support us</h4>
+        <p><a href="#">Privacy</a></p>
+        <p><a href="#">Terms</a></p>
+        <p><a href="#">Cookie Policy</a></p>
+        <p><a href="#">Sitemap</a></p>
+      </div>
+      <div class="footer-department">
+        <h4>Explore</h4>
+        <p><a href="#">Accommodation</a></p>
+        <p><a href="#">Art Academics</a></p>
+        <p><a href="#">Central Services</a></p>
+        <p><a href="#">Conferences and Events</a></p>
+        <p><a href="#">Food and Drink</a></p>
+      </div>
+      <div class="footer-department">
+        <h4>Visit</h4>
+        <p><a href="#">Admission</a></p>
+        <p><a href="#">Contact us</a></p>
+        <p><a href="#">FAQs</a></p>
+        <p><a href="#">Request Information</a></p>
+        <p><a href="#">Privacy Notice</a></p>
+      </div>
+    </div>
+  </footer>
+  <footer class="footer2">
+    <div class="footer2-bar">
+      <div class="footer2-copyright">
+        &copy; 2020 tophive. All rights Reserved.
+      </div>
+      <div class="footer2-links">
+        <a href="#">Privacy</a>
+        <a href="#">Terms</a>
+        <a href="#">Cookie Policy</a>
+        <a href="#">Sitemap</a>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <style scoped>
+  body {
+      margin: 0;
+      padding: 0;
+      font-family: Arial, sans-serif;
+    }
+    
+    footer {
+      background-color: #23374B;
+      color: #fff;
+      padding: 20px;
+      text-align: center;
+    }
+    
+    .footer-content {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+      align-items: center;
+      gap: 20px;
+    }
+    
+    .footer-text {
+      flex: 2;
+      text-align: right;
+      font-size: 14px;
+      line-height: 1.5;
+    }
+    
+    .footer-button {
+      background-color: #fff;
+      color: #23374B;
+      padding: 10px;
+      border: none;
+      border-radius: 5px;
+      font-weight: bold;
+      text-transform: uppercase;
+      text-decoration: none;
+    }
+    
+    .footer-departments {
+      margin-top: 30px;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      color: #555555;
+    }
+    
+    .footer-department {
+      width: 250px;
+      margin: 0 10px;
+      color: #555555;
+    }
+    
+    .footer-department h4 {
+      font-size: 18px;
+      margin-bottom: 10px;
+      color: whitesmoke;
+    }
+    
+    .footer-department p {
+      font-size: 14px;
+      margin-bottom: 8px;
+      color: #777777;;
+    }
+    
+    .footer-department a {
+      color: #fff;
+      text-decoration: none;
+      color: #777777;
+    }
 
+    
+
+    
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    .footer2 {
+      background-color: #23374B;
+      color: #fff;
+      padding: 20px;
+      text-align: center;
+    }
+    
+    .footer2-content {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+      align-items: center;
+      gap: 20px;
+    }
+    
+    .footer2-text {
+      flex: 2;
+      text-align: right;
+      font-size: 14px;
+      line-height: 1.5;
+    }
+    
+    .footer2-button {
+      background-color: #fff;
+      color: #23374B;
+      padding: 10px;
+      border: none;
+      border-radius: 5px;
+      font-weight: bold;
+      text-transform: uppercase;
+      text-decoration: none;
+    }
+    
+    .footer2-bar {
+      background-color: #192735;
+      height: 2rem;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0 20px;
+    }
+    
+    .footer2-copyright {
+      font-size: 14px;
+      margin-left: 4rem;
+      color: #555555;
+    }
+    
+    .footer2-links {
+      font-size: 14px;
+      
+    }
+    
+    .footer2-links a {
+      color: #fff;
+      text-decoration: none;
+      margin-left: 10px;
+      margin-right: 4rem;
+      color: #555555;
+    }
+
+
+
+
+    
 </style>
